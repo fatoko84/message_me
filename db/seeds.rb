6e7668498fb6863a8229621ee6fd5758ef24e215
Tobias Weinmann <weinmann.t@outlook.com>
2020-05-11 12:10:40 +0200

    Create User Model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Tobias", password: "password")
+User.create(username: "Sophie", password: "password")
+User.create(username: "Hanna", password: "password")
+User.create(username: "Sdu", password: "password")
+User.create(username: "Schnatt", password: "password")
