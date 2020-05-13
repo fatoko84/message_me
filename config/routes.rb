e81c118dbbc36b09f42c34875104239c2f465171
Tobias Weinmann <weinmann.t@outlook.com>
2020-05-13 15:43:27 +0200

    Add method to braodcast messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'chatroom#index'
   get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  post 'messages', to: 'messages#create'
 
-  resources :users
-  resources :messages
+  mount ActionCable.server, at: '/cable'
 end
